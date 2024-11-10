22b67bf68227559578b90e3b1ce52ed0905d36be
Maharshi309 <23dcs080@charusat.edu.in>
2024-11-10 21:22:28 +0530

    Change 1

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
+
+
 // Route for registering a new user
 router.post('/register', async (req, res) => {
     try {
